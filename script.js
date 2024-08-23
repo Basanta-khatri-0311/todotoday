795f37d596d803930b778ad39fd9726c5534a089
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-08-24 01:31:57 +0545

    making the webpage dynamic by adding the js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// const tick =
+const allTickBoxes = document.querySelectorAll('.radio-btn')
+const inputFields = document.querySelectorAll('.goal-input')
+const toggleWarning = document.querySelector('.warning')
+const valueForProgressBar = document.querySelector('.progress-value')
+
+allTickBoxes.forEach((checkbox) => {
+  checkbox.addEventListener('click', (e) => {
+    const checkFilled = [...inputFields].every((input) => input.value)
+
+    if (checkFilled) {
+      checkbox.parentElement.classList.toggle('completed')
+      valueForProgressBar.style.width ='33.3%'
+    }else{
+      toggleWarning.style.visibility = 'visible'
+    }
+  })
+})
+
+inputFields.forEach((input)=>{
+  input.addEventListener('focus',()=>{
+    toggleWarning.style.visibility = 'hidden'
+  })
+})
